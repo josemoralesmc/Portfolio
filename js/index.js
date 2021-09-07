9bb9e89dea5e3da46b86bc89c0c474bef8d8ffb7
josemoralesmc <josemorales270102@gmail.com>
2021-09-07 18:30:12 -0300

    Menu Hamburgusa (estructura)

---
+++ js/index.js
@@ -0,0 +1,78 @@
+     
+//  SCROLL SUAVE
+
+window.onload = () => {
+    let links = document.querySelectorAll('a')
+    
+    console.log(links);
+
+    let primero = links[0]
+    primero.addEventListener('click', () => {
+      scrollSuave('#Home', 500, 81)
+    })
+  
+    let segundo = links[1]
+    segundo.addEventListener('click', () => {
+      scrollSuave('#SectionAboutMe', 500, 10)
+    })
+  
+    let tercero = links[2]
+    tercero.addEventListener('click', () => {
+      scrollSuave('#Proyects', 500, 10)
+    })
+    let cuarto = links[3]
+    cuarto.addEventListener('click', () => {
+      scrollSuave('#Services', 500, 10)
+    })
+    let quinto = links[4]
+    quinto.addEventListener('click', () => {
+      scrollSuave('#Contact', 500, 10)
+    })
+    let sexto = links[5]
+    sexto.addEventListener('click', () => {
+      scrollSuave('#Contact', 500, 10)
+    })
+    let septimo = links[6]
+    septimo.addEventListener('click', () => {
+      scrollSuave('#SectionAboutMe', 500, 10)
+    })
+  }
+  
+  
+  const scrollSuave = (objetivo, duracion, compensacion) => {
+    let elemObj = document.querySelector(objetivo)
+    let elemPos = elemObj.getBoundingClientRect().top - compensacion
+    let posInicial = window.pageYOffset
+    let tiempoInicial = null
+  
+    const animacion = tiempoAhora => {
+      if (tiempoInicial === null) tiempoInicial = tiempoAhora
+      tiempoPasado = tiempoAhora - tiempoInicial
+      let auxAnimacion = easeInOutQuad(tiempoPasado, posInicial, elemPos, duracion)
+      window.scrollTo(0, auxAnimacion)
+      if (tiempoPasado < duracion) requestAnimationFrame(animacion)
+    }
+    requestAnimationFrame(animacion)
+  }
+  
+  const easeInOutQuad = (t, b, c, d) => {
+    t /= d / 2
+    if (t < 1) return c / 2 * t * t + b
+    t--
+    return - c / 2 * (t * (t - 2) - 1) + b
+  }
+
+  //MENU HAMBURGUESA
+  
+  let svghamburguer = document.getElementById("svghamburguer");
+  let ulList = document.getElementById("ulList");
+
+  svghamburguer.addEventListener("click", (e) =>{
+      let src = e.target.getAttribute("src");
+      if (src == "./img/menu.svg") {
+          e.target.setAttribute("src", "./img/close.svg");
+        }else {
+          e.target.setAttribute("src", "./img/menu.svg");
+      }
+    ulList.classList.toggle("ulItem")
+  })
